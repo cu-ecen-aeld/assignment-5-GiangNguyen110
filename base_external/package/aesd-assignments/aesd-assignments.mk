c7b460ae7d9bd41223140fef2c9594935a70b618
GiangNguyen110 <nguyen.tgiang97@gmail.com>
2025-09-15 17:08:31 +0700

    dummy commit to trigger runner

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -18,7 +18,7 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all	
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+# TODO add your aesdsocket and aesdsocket-start-stop utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5-buildroot/* $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
